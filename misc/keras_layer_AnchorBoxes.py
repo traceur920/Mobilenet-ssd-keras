b02dd6639afef4609a3b91344354366a948e3b0b
simone <simone@homen53sv.lan>
2021-02-03 10:15:24 +0000

    fix: compatibility issue with `image_data_format()`
    Function from tf.keras.backend.image_data_format(), from previous
    keras.backend.image_dim_ordering(), requires arguments to be either
    'channels_first' (as for Theano backend) or 'channels_last' (for
    Tensorflow  backend).

--- misc/keras_layer_AnchorBoxes.py
+++ misc/keras_layer_AnchorBoxes.py
@@ -174,7 +174,7 @@ class AnchorBoxes(Layer):
         wh_list = np.array(wh_list)
 
         # We need the shape of the input tensor
-        if K.image_data_format() == 'tf':
+        if K.image_data_format() == 'channels_last':
             batch_size, feature_map_height, feature_map_width, feature_map_channels = x.shape
         else: # Not yet relevant since TensorFlow is the only supported backend right now, but it can't harm to have this in here for the future
             batch_size, feature_map_channels, feature_map_height, feature_map_width = x.shape
